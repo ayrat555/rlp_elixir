53f4729cc196305e7649c0f946a50f0ed7dfc7a1
Ayrat Badykov <ayratin555@gmail.com>
2017-06-08 23:14:43 +0300

    initial commit

---
+++ lib/rlp_elixir.ex
@@ -0,0 +1,18 @@
+defmodule RlpElixir do
+  @moduledoc """
+  Documentation for RlpElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RlpElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rlp_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule RlpElixirTest do
+  use ExUnit.Case
+  doctest RlpElixir
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
